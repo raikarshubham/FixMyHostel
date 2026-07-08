a613fb5a9d48fd8e95ed69c9e58610f8214be3c3
raikarshubham <raikarshubhamvaikunt@gmail.com>
2026-07-09 02:41:53 +0530

    css fixx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
-import "./styles/responsive.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
